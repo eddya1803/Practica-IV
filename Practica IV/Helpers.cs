e13f5d569450be65a45db8c66864f0a4ca46e44c
Eddy Abreu H <60908394+eddya1803@users.noreply.github.com>
2021-09-27 09:48:02 -0400

    Agregar archivos de proyecto.

---
+++ Practica IV/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_IV
+{
+    public static class Helpers
+    {
+
+        public static List<int> Pares(this List<int> num)
+        {
+
+            List<int> NumPares = num.FindAll(n => n % 2 == 0);
+            return NumPares;
+
+        }
+
+    }
+    
+}
---
+++ Practica IV/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica_IV
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numeros = new List<int>() { 2, 3, 5, 9, 11, 14, 15, 18, 19, 22, 25, 27, 30 };
+
+            Console.WriteLine(Helpers.Pares(numeros));
+
+            Console.ReadKey();
+
+        }
+
+        /*=================================================================================================*/
+
+        // 2da. parte de la practica:
+        // Escribir los siguientes metodos como expresiones lambda:
+
+        /* 
+         * a)
+         *    public bool EsImpar(int x){
+                if (x % 2 != 0){
+                  return true;
+                else{
+                  return false;
+                }
+              }
+         */
+
+        public bool EsImpar(int x) => (x % 2 != 0);
+
+
+
+        /*
+         * b)
+        *   public double CalcularITBIS(double precio){
+               var itebis = precio * 0.18;
+               return itebis;
+            }
+        */
+
+        public double CalcularITBIS(double precio) => (precio * 0.18);
+
+
+
+        /*
+         * c)
+        *   public void GetDate(){
+               var fecha = DateTime.Now;
+               return fecha;
+            }
+        */
+
+        public DateTime GetDate() => DateTime.Now;
+      
+    }
+
+}
